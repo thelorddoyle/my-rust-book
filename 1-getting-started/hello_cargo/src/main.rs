93876e363839cf61c5f269183857c208de1a37f1
Daniel Lord-Doyle <dlorddoyle@vhslab.com>
2023-05-11 13:01:40 +1000

    add hello cargo

--- 1-getting-started/hello_cargo/src/main.rs
+++ 1-getting-started/hello_cargo/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, cargo!");
 }
+
+// cargo notes
+
+// 1. check version with cargo --version
+// 2. command to create cargo project is cargo new
+// 3. command to build and obtain exec binary is cargo build
+// 4. to build and run same line it is cargo run
+// 5. to check it can compile but NOT create a binary executable is cargo check
+// 6. if there are no changes to codebase it will not compile (cool!)
+// 7. to build for release use cargo build --release
+// 8. can set different compiling rules / optimisations between development and production
